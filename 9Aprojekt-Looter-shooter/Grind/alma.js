67a8cb661744f221bcdf1f531db592c6bd9a8738
Lesummer <144891846+TailorCornedge@users.noreply.github.com>
2024-04-21 19:16:36 +0200

    Add files via upload

---
+++ 9Aprojekt-Looter-shooter/Grind/alma.js
@@ -0,0 +1,11 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-slider', {
+          cover      : true,
+          heightRatio: 0.5,
+    } ).mount();
+  } );
+
+
+  <div class="slide active"><img src="./images/14516432428_b6f206c4f7_b.jpg" alt="Image 1"></div>
+  <div class="slide"><img src="./images/28169680598_98f82498bd_b.jpg" alt="Image 2"></div>
+  <div class="slide"><img src="./images/49100451222_711bfab435_b.jpg" alt="Image 3"></div>
---
+++ 9Aprojekt-Looter-shooter/README.md
@@ -0,0 +1,3 @@
+# 9Aprojekt
+
+https://trello.com/invite/b/oTNsI2IC/ATTId17d2212c5ba01a9a41f94ad9a44bdedB3D97B49/9aprojekt
