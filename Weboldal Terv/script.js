34716f3211b9df42d86d7c44ec5cbb3449e6158e
DarklDrift1 <144444160+DarklDrift1@users.noreply.github.com>
2024-04-22 08:41:52 +0200

    Add files via upload

--- Weboldal Terv/script.js
+++ Weboldal Terv/script.js
@@ -1,23 +1,27 @@
 
 
-function pagefunction(page) {
+function pagefunction(page)
+{
     page_header = document.getElementById("header")
     page_games = document.getElementById("games")
     page_funny = document.getElementById("funny")
 
-    if(page == header){
+    if(page == header)
+    {
         page_header.style.display = "block";
         page_games.style.display = "none";
         page_funny.style.display = "none";
     }
     
-    else if(page == games) {
+    else if(page == games) 
+    {
         page_header.style.display = "none";
         page_games.style.display = "block";
         page_funny.style.display = "none";
     }
 
-    else if (page === undefined) {
+    else if (page === undefined) 
+    {
         page_header.style.display = "none";
         page_games.style.display = "none";
         page_funny.style.display = "block";
@@ -27,18 +31,22 @@ function pagefunction(page) {
 function cardfunction(hover, game) {
     sel_game = document.getElementById(game)
     var modal = document.getElementById(game + "_modal")
-    if(hover == 1) {
+    if(hover == 1) 
+    {
         sel_game.style.background = "rgba(0,0,0,0.3)";
     }
-    else if (hover == 2 || hover === undefined) {
+    else if (hover == 2 || hover === undefined) 
+    {
         sel_game.style.background = "rgba(0,0,0,0)";
     }
     else if (hover == 3)
     {
-        if(modal.style.display == "block") {
+        if(modal.style.display == "block") 
+        {
             modal.style.display = "none"
         }
-        else{
+        else
+        {
             modal.style.display = "block"
         }
     }
