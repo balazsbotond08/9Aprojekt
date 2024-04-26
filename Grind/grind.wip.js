95870268ecf3ca90f451ccab2e756d82bbabe79d
Lesummer <144891846+TailorCornedge@users.noreply.github.com>
2024-04-26 09:37:53 +0200

    Add files via upload

--- Grind/grind.wip.js
+++ Grind/grind.wip.js
@@ -1,5 +1,5 @@
 function letsgo (){
-    alert("jelszó: "+pw.value)
+    alert("jelszó: "+pw.value + "köszike, ez hazsnos lesz :)")
     localStorage.setItem("email", mail.value)
     localStorage.setItem("pass", pw.value)
     localStorage.clear
