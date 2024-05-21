14eae270009bb0961527aaa1ccc6aef628349ed1
SolteszZoltan <144891419+SolteszZoltan@users.noreply.github.com>
2024-05-21 09:18:42 +0200

    Add files via upload

---
+++ soltesz/hirlevel.js
@@ -0,0 +1,14 @@
+document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var email = document.getElementById('email').value;
+  if (!email.includes('@')) {
+      alert('Kérjük, érvényes email címet adjon meg.');
+      return;
+  }
+  document.getElementById('message').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('message').classList.add('popup-on-overlay');
+  setTimeout(function() {
+      window.location.href = 'kaland.html';
+  }, 2000); 
+});
