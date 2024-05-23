8ac4dbd695180468ce5d3eb3e7d5c57a5ed6e41e
DarklDrift1 <144444160+DarklDrift1@users.noreply.github.com>
2024-05-23 22:14:11 +0200

    a

--- nagym/strategy_script.js
+++ nagym/strategy_script.js
@@ -38,3 +38,16 @@ function cardfunction(hover, game) {
         }
     }
 }
+
+function hirlevelFunction() {
+    var vezeteknev = document.getElementById("vezeteknev").value;
+    var keresztnev = document.getElementById("keresztnev").value;
+  
+
+    if (vezeteknev == "" || keresztnev == "" || email == "") {
+      alert("Kérjük, töltse ki az összes mezőt!");
+      return false;
+    }
+    alert("Köszönjük, hogy feliratkozott a hírlevelünkre!");
+    return false;
+  }
