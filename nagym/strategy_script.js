345564497f30d7fee2b09c4a2c379f00bcd7d7e2
DarklDrift1 <144444160+DarklDrift1@users.noreply.github.com>
2024-05-23 23:00:52 +0200

    Add files via upload

--- nagym/strategy_script.js
+++ nagym/strategy_script.js
@@ -38,16 +38,3 @@ function cardfunction(hover, game) {
         }
     }
 }
-
-function hirlevelFunction() {
-    var vezeteknev = document.getElementById("vezeteknev").value;
-    var keresztnev = document.getElementById("keresztnev").value;
-  
-
-    if (vezeteknev == "" || keresztnev == "" || email == "") {
-      alert("Kérjük, töltse ki az összes mezőt!");
-      return false;
-    }
-    alert("Köszönjük, hogy feliratkozott a hírlevelünkre!");
-    return false;
-  }
