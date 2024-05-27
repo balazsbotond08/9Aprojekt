31d52b9ab8e7e6b08068e98e397bc49e00f63982
Lesummer <144891846+TailorCornedge@users.noreply.github.com>
2024-05-27 08:09:31 +0200

    Delete 9Aprojekt-Looter-shooter directory

--- 9Aprojekt-Looter-shooter/Grind/grind.wip.js
+++
@@ -1,6 +0,0 @@
-function letsgo (){
-    alert("jelszó: "+pw.value + "köszike, ez hazsnos lesz :)")
-    localStorage.setItem("email", mail.value)
-    localStorage.setItem("pass", pw.value)
-    localStorage.clear
-}
--- 9Aprojekt-Looter-shooter/Grind/web/grind.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '#image-slider', {
-          cover      : true,
-          heightRatio: 0.5,
-    } ).mount();
-  } );
-
